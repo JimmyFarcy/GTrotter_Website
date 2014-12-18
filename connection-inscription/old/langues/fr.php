dbe291a6991be6d46962585a4f2e3f10faf6bf1a
Cedric Guerrier <cedric.guerrier.932609@gmail.com>
2014-12-18 22:49:11 +0800

    Connection / Inscription (old and new) - Cedric

---
+++ connection-inscription/old/langues/fr.php
@@ -0,0 +1,34 @@
+<?php
+	/*
+	 * Langue: francaise
+	 * Défini comme le fichier de langue par défaut
+	 */
+
+	
+	$connexion1 = "nom&nbsp;d'&nbsp;utilisateur";
+	$connexion2 = "mot&nbsp;de&nbsp;passe";
+	$connexion3 = "Connexion";
+	$connexion4 = "Inscription";
+	$connexion5 = "vous avez oubli&eacute; votre mot de passe?";
+	
+	$deconnexion = "Se d&eacute;connecter";
+	
+	$error_connexion1 = "Votre pseudonyme ou votre mot de passe est incorrect.";
+	$error_connexion2 = "Votre mot de passe doit contenir au minimum 6 caract&egrave;res.";
+	$error_connexion3 = "Ce pseudonyme ou cette adresse mail existe d&eacute;j&agrave;.";
+	$error_connexion4 = "Vous avez mal retap&eacute; votre mot de passe.";
+	
+	$inscription1 = "Nom&nbsp;d'utilisateur";
+	$inscription2 = "Mot&nbsp;de&nbsp;passe";
+	$inscription3 = "Retaper&nbsp;le&nbsp;mot&nbsp;de&nbsp;passe";
+	$inscription4 = "Adresse&nbsp;mail";
+	$inscription5 = "Valider";
+	
+	$parametre1 = "Pseudonyme";
+	$parametre2 = "Mot&nbsp;de&nbsp;passe";
+	$parametre3 = "Nouveau&nbsp;pseudonyme";
+	$parametre4 = "Retaper&nbsp;le&nbsp;mot&nbsp;de&nbsp;passe";
+	$parametre5 = "Nouveau&nbsp;mot&nbsp;de&nbsp;passe";
+	$parametre6 = "Enregistrer";
+	
+?>
