fb4e721e763f8fe14ece115f71be524b9aa776fd
Cedric Guerrier <cedric.guerrier.932609@gmail.com>
2014-12-25 16:06:53 +0800

    Remove des anciens dossiers.

--- connection-inscription/old/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-     $host = "127.0.0.1";
-     $user = "root";
-     $bdd = "";
-      $passwd = "";                   
-      mysql_connect($host, $user,$passwd) or die("erreur de connexion au serveur");
-      mysql_select_db($bdd) or die("erreur de connexion a la base de donnees");
-?>
